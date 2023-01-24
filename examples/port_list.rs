fa9f1c5292fdd1296dbe7f5743df9f2b2e59a04f
Ashcon Mohseninia <ashconm@outlook.com>
2023-01-24 13:10:46 +0100

    Add VID/PID report for serial Linux

--- examples/port_list.rs
+++ examples/port_list.rs
@@ -1,33 +1,72 @@
-use std::io::{Read, BufReader, BufRead};
+use std::io::{Read, BufReader, BufRead, Write, BufWriter};
 
-use pyserial_rs::{windows::{port_lister, COMPort}, PortScanner, SerialPortState};
+use serial_rs::{PortScanner, SerialPortSettings, FlowControl, SerialPort};
 
+#[cfg(windows)]
+use serial_rs::{windows::{port_lister, COMPort}};
+
+#[cfg(unix)]
+use serial_rs::posix::{TTYPort, port_lister};
 
 fn main() {
-    let mut scanner = port_lister::COMPortLister{};
-    for port in scanner.list_devices().unwrap() {
-        println!("Found port:");
-        println!("\tPort: {}", port.get_port());
-        println!("\tDescription: {}", port.get_desc());
-        println!("\tManufacturer: {}", port.get_manufacturer());
+
+    #[cfg(windows)]
+    {
+        let mut scanner = port_lister::COMPortLister{};
+        for port in scanner.list_devices().unwrap() {
+            println!("Found port:");
+            println!("\tPort: {}", port.get_port());
+            println!("\tDescription: {}", port.get_desc());
+            println!("\tManufacturer: {}", port.get_manufacturer());
+        }
     }
 
-    match COMPort::new("COM4".into(), Some(
-        SerialPortState::default()
+    #[cfg(unix)]
+    {
+        let mut scanner = port_lister::TTYPortScanner{};
+        for port in scanner.list_devices().unwrap() {
+            println!("Found port:");
+            println!("\tPort: {}", port.get_port());
+            println!("\tDescription: {}", port.get_desc());
+            println!("\tManufacturer: {}", port.get_manufacturer());
+        }
+    }
+
+    #[cfg(windows)]
+    let p = COMPort::new("COM7".into(), Some(
+        SerialPortSettings::default()
             .baud(115200)
             .read_timeout(Some(100))
-            .dsr_dtr(false)
-    )) {
+            .write_timeout(Some(100))
+            .set_flow_control(FlowControl::None)
+    ));
+    #[cfg(unix)]
+    let p = TTYPort::new("/dev/ttyUSB0".into(), Some(
+        SerialPortSettings::default()
+            .baud(115200)
+            .read_timeout(Some(100))
+            .write_timeout(Some(100))
+            .set_flow_control(FlowControl::None)
+    ));
+    match p {
         Ok(mut port) => {
+            let clone_r = port.try_clone().unwrap();
+            let mut clone_w = port.try_clone().unwrap();
             println!("Port open OK!");
-            let mut buf_reader = BufReader::new(&mut port);
+            let test_msg: &[u8] = "#07E11092\n".as_bytes();
+            let mut buf_reader = BufReader::new(clone_r);
             let mut b = String::new();
             loop {
                 if buf_reader.read_line(&mut b).is_ok() {
-                    print!("{}", b);
+                    print!("IN : {}", b);
                     b.clear();
+                    println!("OUT: {:02X?}", test_msg);
+                    if let Err(e) = clone_w.write(test_msg) {
+                        eprintln!("Write error {}", e)
+                    }
+                } else {
+                    std::thread::sleep(std::time::Duration::from_millis(50));
                 }
-                std::thread::sleep(std::time::Duration::from_millis(50));
             }
         },
         Err(e) => {
--- src/posix/port_lister.rs
+++ src/posix/port_lister.rs
@@ -1,6 +1,6 @@
 //! Linux TTY port lister and enumerator
 
-use std::path::PathBuf;
+use std::{path::PathBuf, fs::File, io::Read};
 
 use crate::PortInfo;
 
@@ -24,24 +24,47 @@ fn get_paths(g: &str) -> Vec<PathBuf> {
 impl crate::PortScanner for TTYPortScanner {
     fn list_devices(&mut self) -> crate::SerialResult<Vec<crate::PortInfo>> {
         let mut res: Vec<PortInfo> = vec![];
-        for port in get_paths("/dev/ttyS*").into_iter()
-            .chain(get_paths("/dev/ttyUSB*"))
-            .chain(get_paths("/dev/ttyXRUSB*"))
-            .chain(get_paths("/dev/ttyACM*"))
-            .chain(get_paths("/dev/ttyAMA*"))
-            .chain(get_paths("/dev/rfcomm*"))
-            .chain(get_paths("/dev/ttyAP*"))
-            .chain(get_paths("/dev/ttyGS*")) 
-            .chain(get_paths("/dev/cu*")) // OSX
+        let mut pat = get_paths("/dev/ttyS*").into_iter()
+        .chain(get_paths("/dev/ttyUSB*"))
+        .chain(get_paths("/dev/ttyXRUSB*"))
+        .chain(get_paths("/dev/ttyACM*"))
+        .chain(get_paths("/dev/ttyAMA*"))
+        .chain(get_paths("/dev/rfcomm*"))
+        .chain(get_paths("/dev/ttyAP*"))
+        .chain(get_paths("/dev/ttyGS*"));
+        #[cfg(macos)]
+        {
+            pat = pat.chain(get_paths("/dev/cu*")) // OSX
+        }
+        for port in pat
         {
             let dev_name = port.to_str().unwrap().split("/").last().unwrap();
 
             let mut path: Option<PathBuf> = None;
             let mut subsystem: Option<PathBuf> = None;
-
+            let mut port_info = PortInfo::default();
+                    
             if PathBuf::from(format!("/sys/class/tty/{dev_name}/device")).exists() {
                 path = Some(std::fs::canonicalize(format!("/sys/class/tty/{dev_name}/device")).unwrap());
                 subsystem = std::fs::canonicalize(format!("{}/subsystem", path.clone().unwrap().to_str().unwrap())).ok();
+                if let Ok(mut f) = File::open(format!("/sys/class/tty/{dev_name}/device/uevent")) {
+                    let mut s = String::new();
+                    f.read_to_string(&mut s);
+                    for line in s.lines() {
+                        if line.starts_with("PRODUCT=") {
+                            let p = line.replace("PRODUCT=", "");
+                            let parts: Vec<&str> = p.split("/").collect();
+                            if parts.len() == 3 {
+                                if let Ok(vid) = u16::from_str_radix(parts[0], 16) {
+                                    port_info.vid = vid;
+                                }
+                                if let Ok(pid) = u16::from_str_radix(parts[1], 16) {
+                                    port_info.pid = pid;
+                                }
+                            }
+                        }
+                    }
+                }
             }
             
             //let mut usb_interface_path: Option<PathBuf> = None;
@@ -55,7 +78,6 @@ impl crate::PortScanner for TTYPortScanner {
                 }
             }
 
-            let mut port_info = PortInfo::default();
             port_info.port = port.to_string_lossy().to_string();
 
 
